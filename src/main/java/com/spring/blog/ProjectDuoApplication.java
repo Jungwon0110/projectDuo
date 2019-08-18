cc83432ea1a713f2df9d66696c819768ea6e2f84
jeongwon <fffff4324@gmail.com>
2019-08-18 17:31:45 +0900

    first commit

---
+++ src/main/java/com/spring/blog/ProjectDuoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDuoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/spring/blog/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.spring.blog;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectDuoApplication.class);
+	}
+
+}
