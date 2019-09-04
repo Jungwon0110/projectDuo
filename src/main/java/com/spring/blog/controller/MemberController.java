2cdb3d62b98e63b406bd034d96bcef60abf14c0c
박정원 <fffff0110@naver.com>
2019-09-04 10:32:23 +0900

    change role guest to user after confirm

--- src/main/java/com/spring/blog/controller/MemberController.java
+++ src/main/java/com/spring/blog/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.spring.blog.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
+import javax.mail.MessagingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -12,30 +15,65 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.spring.blog.domain.Account;
-import com.spring.blog.service.EmailServiceImpl;
+import com.spring.blog.dto.AccountKeyDto;
+import com.spring.blog.dto.TempKey;
+import com.spring.blog.service.MailServiceImpl;
 import com.spring.blog.service.MemberService;
 
 @Controller
 public class MemberController {
-	
-	@Resource(name="com.spring.blog.service.MemberService")
-	@Autowired
+   
+   @Resource(name="com.spring.blog.service.MemberService")
+   @Autowired
     MemberService memberService;
-	@Autowired
-	JavaMailSender javaMailSender;
-	
-	@RequestMapping("/find/{email}")
-	@ResponseBody
-	private Account findMemberByEmail(@PathVariable String email) throws Exception {
-		Account existmem = memberService.findMemberByEmail(email);
-		return existmem;
-	}
-	
-	@RequestMapping(value="mail", method = RequestMethod.POST)
-	public String Mail(HttpServletResponse req) {
-		EmailServiceImpl email = new EmailServiceImpl();
-		email.setJavaMailSender(javaMailSender);
-		email.sendSimpleMessage("메일주소", "제목", "내용");
-		return "redirect:/";
-	}
+   @Autowired
+   JavaMailSender javaMailSender;
+   
+   @RequestMapping("/find/{email}")
+   @ResponseBody
+   private Account findMemberByEmail(@PathVariable String email) throws Exception {
+      Account existmem = memberService.findMemberByEmail(email);
+      return existmem;
+   }
+   
+   
+   @RequestMapping(value = "/emailConfirm/{email}", method = RequestMethod.POST)
+   @ResponseBody
+   public String sendMailAuth(@PathVariable String email) throws MessagingException, UnsupportedEncodingException {
+      MailServiceImpl sendmail = new MailServiceImpl(javaMailSender);
+      
+      String key = new TempKey().getKey(50,false);
+      
+      AccountKeyDto actkey = new AccountKeyDto();
+      actkey.setEmail(email);
+      actkey.setAuthkey(key);
+      
+      memberService.insertKey(actkey);
+
+      sendmail.setSubject("[이메일 인증]");
+      sendmail.setText(
+               new StringBuffer().append("<h1>메일인증</h1>")
+               .append("<a href='http://localhost:8080/emailConfirm?email=")
+               .append(email)
+               .append("&authKey=").append(key)
+               .append("' target='_blank'>이메일 인증 확인</a>").toString());      
+      sendmail.setFrom("fffff4324@gmail.com", "projectDuo");
+      sendmail.setTo(email);
+      sendmail.send();
+      return "/";
+   }
+   
+   @RequestMapping(value = "/emailConfirm/", method = RequestMethod.GET)
+   @ResponseBody
+   public String confirmMember(@PathVariable String email, @PathVariable String authkey) throws Exception {
+      String userkey = memberService.findKey(email);
+      if(userkey.equals(authkey)) {
+         System.out.println("인증 성공!!!");
+         memberService.changeRole(email);
+      }else {
+         
+      }
+      return "/";
+   }
+   
 }
--- src/main/java/com/spring/blog/mapper/MemberMapper.java
+++ src/main/java/com/spring/blog/mapper/MemberMapper.java
@@ -1,17 +1,22 @@
 package com.spring.blog.mapper;
 
-import java.util.HashMap;
-
 import org.springframework.stereotype.Repository;
 
 import com.spring.blog.domain.Account;
+import com.spring.blog.dto.AccountKeyDto;
 
 @Repository("com.spring.blog.mapper.MemberMapper")
 public interface MemberMapper {
 
-	//이메일로 유저찾기
+   //이메일로 유저찾기
     public Account findMemberByEmail(String email) throws Exception;
     
     //key값 넣어주기
-    public void insertKey(HashMap<String, String> parameters);
+    public void insertKey(AccountKeyDto actkey);
+    
+    //key값 비교 
+    public String findKey(String email);
+    
+    //role 변경
+    public void changeRole(String email);
 }
